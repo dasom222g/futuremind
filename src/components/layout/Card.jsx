708a41e6c060bbdf641c551d23371e74acf45177
dasom222g <dasom228@gmail.com>
2024-10-17 02:53:54 +0900

    Add card component

---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { hoverTopMotion } from "../../styles/style";
+
+const Card = ({ data }) => {
+  // logic
+  const { title, titleColor, icon, itemList } = data;
+  // view
+  return (
+    <li
+      className={`w-full rounded-3xl px-12 py-16 shadow-[inset_2px_2px_3px_rgba(255,255,255,0.1),inset_-2px_-2px_3px_rgba(255,255,255,0.1)] bg-[linear-gradient(135deg,rgba(255,255,255,0.03)_0%,rgba(0,0,0,0.5)_100%)] ${hoverTopMotion}`}
+    >
+      <h2
+        className={`text-44 font-semibold whitespace-pre-wrap text-${titleColor}`}
+      >
+        {title}
+      </h2>
+      <div className="absolute top-7 right-7">
+        <img
+          src={`./images/icon/${icon}.png`}
+          alt="아이콘 이미지"
+          className="w-[46px] h-auto"
+        />
+      </div>
+      <ul className="pt-12 flex flex-col gap-9">
+        {itemList.map((item, index) => (
+          <li key={`item-${index}`}>
+            <h3 className="text-2xl font-semibold">{item.subtitle}</h3>
+            <p className="text-xl text-future-gray-200 pt-3 break-keep">
+              {item.description}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default Card;
