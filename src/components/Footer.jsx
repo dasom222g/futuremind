0088b5755125f073aff0d020bc6ed11c0b86fe12
dasom222g <dasom228@gmail.com>
2024-10-17 16:21:20 +0900

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { contactList } from "../data/reponse";
 
 const Footer = () => {
+  // logic
+
+  // view
   return (
-    <footer className="bg-black text-white py-10">
-      <div className="container mx-auto text-center">
-        <p>© 2024 futuremind All Rights Reserved.</p>
-      </div>
+    <footer className="bg-black bg-opacity-30 text-white py-2 flex pl-[160px] pr-20">
+      <h2>
+        <img src="./images/logo-footer.png" alt="로고 이미지" />
+      </h2>
+      <ul className="flex ml-auto">
+        {contactList.map((contact) => (
+          <li key={contact.id} className="flex items-center text-base group">
+            <i className="block w-px h-3 bg-white mx-2 group-first:hidden"></i>
+            {contact.text}
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 };
