7a2d3475fee3fc91f065a8bde6572daa96373158
dasom222g <dasom228@gmail.com>
2024-10-22 01:34:12 +0900

    Add scrollTop for page move

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useRef } from "react";
 import Header from "./Header";
 import { useSetRecoilState } from "recoil";
 import { scrollYState } from "../../data/state";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  // logic
+  const location = useLocation();
   const scrollAreaRef = useRef(null);
   const setScrollY = useSetRecoilState(scrollYState);
 
   // 스크롤 이벤트 감지
   useEffect(() => {
-    const current = scrollAreaRef.current;
+    const current = scrollAreaRef?.current;
     const handleScroll = () => {
       setScrollY(current.scrollTop);
     };
@@ -21,6 +24,13 @@ const Layout = ({ children }) => {
     };
   }, [setScrollY]);
 
+  useEffect(() => {
+    // 페이지 이동 or 현재페이지 재진입시 새로고침 효과
+    scrollAreaRef?.current &&
+      scrollAreaRef?.current.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname, location.key]);
+
+  // view
   return (
     <div
       ref={scrollAreaRef}
