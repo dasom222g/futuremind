b5a344564a03d890de5ad020a18f54068700b14f
dasom222g <dasom228@gmail.com>
2024-10-24 21:36:33 +0900

    Fix feedback

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ data }) => {
   const { title, titleColor, icon, itemList } = data;
   // view
   return (
-    <li className="w-full rounded-3xl px-12 pt-16 pb-11 max-h-[605px] overflow-y-auto shadow-[inset_2px_2px_3px_rgba(255,255,255,0.1),inset_-2px_-2px_3px_rgba(255,255,255,0.1)] bg-[linear-gradient(135deg,rgba(255,255,255,0.03)_0%,rgba(0,0,0,0.5)_100%)] hover-top">
+    <li className="w-full rounded-3xl px-12 pt-16 pb-11 max-h-[606px] shadow-[inset_2px_2px_3px_rgba(255,255,255,0.1),inset_-2px_-2px_3px_rgba(255,255,255,0.1)] bg-[linear-gradient(135deg,rgba(255,255,255,0.03)_0%,rgba(0,0,0,0.5)_100%)] hover-top">
       <h2
         className={`text-44 font-semibold whitespace-pre-wrap text-${titleColor}`}
         data-aos="fade-up"
