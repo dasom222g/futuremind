8cef7f5778c5bcac6b907213ce802dbb982dc0e2
dasom222g <dasom228@gmail.com>
2024-11-21 14:09:19 +0900

    Fix text style

--- src/components/MainCardMobile.jsx
+++ src/components/MainCardMobile.jsx
@@ -8,14 +8,14 @@ const MainCardMobile = ({ data }) => {
         alt="아이콘 이미지"
         className="w-[46px] h-auto"
       />
-      <p className="text-3xl font-bold text-future-blue-400 py-4 leading-10 mt-1">
+      <p className="text-3xl font-bold text-future-blue-400 py-4 leading-10 mt-1 whitespace-pre-wrap">
         {data.title}
       </p>
       <i className="block bg-white w-full h-[2px] my-8"></i>
       <ul>
         {data.itemList.map((item, index) => (
           <li key={index} className="py-4">
-            <p className="text-2xl whitespace-pre-wrap">{item.subtitle}</p>
+            <p className="text-2xl">{item.subtitle}</p>
             <span className={`block text-base text-gray-200 font-thin mt-4`}>
               {item.description}
             </span>
